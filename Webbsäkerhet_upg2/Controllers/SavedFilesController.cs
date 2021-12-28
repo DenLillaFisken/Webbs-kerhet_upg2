85b2f4035dc27220f373a2ce4e0be916b4898a7e
Alexandra Johansson <alexandra_johansson111@live.se>
2021-12-28 16:14:34 +0100

    filsäkerhet

--- Webbsäkerhet_upg2/Controllers/SavedFilesController.cs
+++ Webbsäkerhet_upg2/Controllers/SavedFilesController.cs
@@ -2,32 +2,102 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Webbsäkerhet_upg2.Data;
 using Webbsäkerhet_upg2.Models;
+using Webbsäkerhet_upg2.Utilities;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace Webbsäkerhet_upg2.Controllers
 {
     public class SavedFilesController : Controller
     {
         private readonly Webbsäkerhet_upg2Context _context;
+        private readonly long fileSizeLimit = 10 * 1048576;
+        private readonly string[] permittedExtensions = { ".jpg" };
 
         public SavedFilesController(Webbsäkerhet_upg2Context context)
         {
             _context = context;
         }
 
-        // GET: SavedFiles
+        // GET: ApplicationFiles
         public async Task<IActionResult> Index()
         {
             return View(await _context.SavedFile.ToListAsync());
         }
 
-        // GET: SavedFiles/Details/5
-        public async Task<IActionResult> Details(Guid? id)
+
+        [HttpPost]
+        [Route(nameof(UploadFile))]
+        public async Task<IActionResult> UploadFile()
+        {
+            var theWebRequest = HttpContext.Request;
+
+            // validation of Content-Type
+            // 1. first, it must be a form-data request
+            // 2. a boundary should be found in the Content-Type
+            if (!theWebRequest.HasFormContentType ||
+                !MediaTypeHeaderValue.TryParse(theWebRequest.ContentType, out var theMediaTypeHeader) ||
+                string.IsNullOrEmpty(theMediaTypeHeader.Boundary.Value))
+            {
+                return new UnsupportedMediaTypeResult();
+            }
+
+            var reader = new MultipartReader(theMediaTypeHeader.Boundary.Value, theWebRequest.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            // This sample try to get the first file from request and save it
+            // Make changes according to your needs in actual use
+            while (section != null)
+            {
+                var DoesItHaveContentDispositionHeader = ContentDispositionHeaderValue.TryParse(section.ContentDisposition,
+                    out var theContentDisposition);
+
+                if (DoesItHaveContentDispositionHeader && theContentDisposition.DispositionType.Equals("form-data") &&
+                    !string.IsNullOrEmpty(theContentDisposition.FileName.Value))
+                {
+                    // Don't trust any file name, file extension, and file data from the request unless you trust them completely
+                    // Otherwise, it is very likely to cause problems such as virus uploading, disk filling, etc
+                    // In short, it is necessary to restrict and verify the upload
+                    // Here, we just use the temporary folder and a random file name
+
+                    SavedFile savedFile = new SavedFile();
+                    savedFile.UntrustedName = HttpUtility.HtmlEncode(theContentDisposition.FileName.Value);
+                    savedFile.TimeStamp = DateTime.UtcNow;
+
+                    savedFile.Content =
+                            await FileHelpers.ProcessStreamedFile(section, theContentDisposition,
+                                ModelState, permittedExtensions, fileSizeLimit);
+                    if (savedFile.Content.Length == 0)
+                    {
+                        return RedirectToAction("Index", "savedFiles");
+                    }
+                    savedFile.Size = savedFile.Content.Length;
+
+                    await _context.SavedFile.AddAsync(savedFile);
+                    await _context.SaveChangesAsync();
+
+                    return RedirectToAction("Index", "savedFiles");
+
+                }
+
+                section = await reader.ReadNextSectionAsync();
+            }
+
+            // If the code runs to this location, it means that no files have been saved
+            return BadRequest("No files data in the request.");
+        }
+
+
+        // GET: ApplicationFiles/Download/5
+        public async Task<IActionResult> Download(Guid? id)
         {
             if (id == null)
             {
@@ -41,84 +111,10 @@ namespace Webbsäkerhet_upg2.Controllers
                 return NotFound();
             }
 
-            return View(savedFile);
+            return File(savedFile.Content, MediaTypeNames.Application.Octet, savedFile.UntrustedName);
         }
 
-        // GET: SavedFiles/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: SavedFiles/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,UntrustedName,TimeStamp,Size,Content")] SavedFile savedFile)
-        {
-            if (ModelState.IsValid)
-            {
-                savedFile.Id = Guid.NewGuid();
-                _context.Add(savedFile);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(savedFile);
-        }
-
-        // GET: SavedFiles/Edit/5
-        public async Task<IActionResult> Edit(Guid? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var savedFile = await _context.SavedFile.FindAsync(id);
-            if (savedFile == null)
-            {
-                return NotFound();
-            }
-            return View(savedFile);
-        }
-
-        // POST: SavedFiles/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(Guid id, [Bind("Id,UntrustedName,TimeStamp,Size,Content")] SavedFile savedFile)
-        {
-            if (id != savedFile.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(savedFile);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SavedFileExists(savedFile.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(savedFile);
-        }
-
-        // GET: SavedFiles/Delete/5
+        // GET: ApplicationFiles/Delete/5
         public async Task<IActionResult> Delete(Guid? id)
         {
             if (id == null)
@@ -136,7 +132,7 @@ namespace Webbsäkerhet_upg2.Controllers
             return View(savedFile);
         }
 
-        // POST: SavedFiles/Delete/5
+        // POST: ApplicationFiles/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(Guid id)
@@ -147,7 +143,7 @@ namespace Webbsäkerhet_upg2.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        private bool SavedFileExists(Guid id)
+        private bool ApplicationFileExists(Guid id)
         {
             return _context.SavedFile.Any(e => e.Id == id);
         }
